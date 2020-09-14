bf4824bc96fd0abe14dce5fc5998c33c18bbe258
Vikranth Apps <imakingapps@gmail.com>
2020-09-14 18:29:33 +0530

    first commit

---
+++ FrameWithStoryB.framework/Headers/FrameWithStoryB.h
@@ -0,0 +1,19 @@
+//
+//  FrameWithStoryB.h
+//  FrameWithStoryB
+//
+//  Created by Vikranth Naidu on 11/09/20.
+//  Copyright © 2020 Vikranth Naidu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameWithStoryB.
+FOUNDATION_EXPORT double FrameWithStoryBVersionNumber;
+
+//! Project version string for FrameWithStoryB.
+FOUNDATION_EXPORT const unsigned char FrameWithStoryBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameWithStoryB/PublicHeader.h>
+
+
